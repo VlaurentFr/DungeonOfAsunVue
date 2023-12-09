e4f6e6e92be11367d27ab008a89ce6134fe20424
Valentin Laurent <laurent.vltn@gmail.com>
2023-12-09 14:07:07 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  base: "/DungeonOfAsunVue/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
