71132122286a35bf807a31eb6f73ab84eca74e83
Valentin Laurent <laurent.vltn@gmail.com>
2024-06-08 03:24:59 +0200

    Updates

--- assets/Gods-4e4c33cb.js
+++
@@ -1,1 +0,0 @@
-import{y as e,i as o}from"./index-05bfe27e.js";const r=e("gods",()=>{const s=o(null),t=o(null);return{god:s,panth:t}});export{r as u};
---
+++ assets/spellbook-66790660.js
@@ -0,0 +1,1 @@
+const s="/DungeonOfAsunVue/assets/swords-f5e43212.png",e="/DungeonOfAsunVue/assets/spellbook-38d32ece.png";export{s as _,e as a};
