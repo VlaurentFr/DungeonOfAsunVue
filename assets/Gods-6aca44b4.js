2d8389b4a91ca88ddbd6b8e9bb39fa1dd96a68a7
Valentin Laurent <laurent.vltn@gmail.com>
2024-06-22 02:46:54 +0200

    Updates

--- assets/Gods-6aca44b4.js
+++
@@ -1,1 +0,0 @@
-import{y as e,h as o}from"./index-d7f9f01a.js";const r=e("gods",()=>{const s=o(null),t=o(null);return{god:s,panth:t}});export{r as u};
---
+++ assets/Gods-7aa3075b.js
@@ -0,0 +1,1 @@
+import{y as e,h as o}from"./index-0dadcb71.js";const r=e("gods",()=>{const s=o(null),t=o(null);return{god:s,panth:t}});export{r as u};
