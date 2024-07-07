e977302ccc33f40b5614c9b7e70904cb24d0c6ef
Valentin Laurent <laurent.vltn@gmail.com>
2024-07-07 07:50:16 +0200

    Updates

---
+++ assets/Gods-6ac595fb.js
@@ -0,0 +1,1 @@
+import{A as e,i as o}from"./index-e68a32a2.js";const r=e("gods",()=>{const s=o(null),t=o(null);return{god:s,panth:t}});export{r as u};
--- assets/Gods-c0d468c7.js
+++
@@ -1,1 +0,0 @@
-import{A as e,i as o}from"./index-313ca04d.js";const r=e("gods",()=>{const s=o(null),t=o(null);return{god:s,panth:t}});export{r as u};
