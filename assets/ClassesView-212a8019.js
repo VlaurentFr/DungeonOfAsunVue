7da58c556f0b1e3275d76b0b4d303ae052d53549
Valentin Laurent <laurent.vltn@gmail.com>
2025-01-05 14:50:03 +0100

    Updates

--- assets/ClassesView-212a8019.js
+++
@@ -1,1 +0,0 @@
-import{i as e,o as c,c as s}from"./index-4d133ff4.js";const o={};function r(t,n){return c(),s("div")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/ClassesView-fad00225.js
@@ -0,0 +1,1 @@
+import{i as e,o as c,c as s}from"./index-ab51dc17.js";const o={};function r(t,n){return c(),s("div")}const _=e(o,[["render",r]]);export{_ as default};
