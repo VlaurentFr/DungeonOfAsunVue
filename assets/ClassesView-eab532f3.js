c03656c8a1cf1e55de279bd47b5f2feb15f19a30
Valentin Laurent <laurent.vltn@gmail.com>
2025-06-07 09:00:35 +0200

    Updates

--- assets/ClassesView-eab532f3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as s}from"./index-082e37d6.js";const o={};function r(t,n){return c(),s("div")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/Gods-35673cb1.js
@@ -0,0 +1,1 @@
+import{I as t,r as o}from"./index-dd043272.js";const n=t("gods",()=>{const r=o(null),s=o(null);return{god:r,panth:s}});export{n as u};
